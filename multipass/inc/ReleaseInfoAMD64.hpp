4555b7e5e4a4ed3cfdafc5c5da9af9615500196f
fpskuji <kujigaviola@gmail.com>
2025-04-20 10:17:41 +0800

    [Implementation] Added function documentation comment and removed unused private member in ReleaseInfoAMD64.hpp. I also updated the cmakelist file to add the corresponding .cpp file

--- multipass/inc/ReleaseInfoAMD64.hpp
+++ multipass/inc/ReleaseInfoAMD64.hpp
@@ -10,20 +10,41 @@ namespace simplestream {
     constexpr std::string_view IMAGE_NAME = "disk1.img";
     constexpr std::string_view LTS = "LTS";
 
+    /**
+     * @brief Class representing release information for AMD64 architecture.
+     */
     class ReleaseInfoAMD64 : public IReleaseInfo {
     public:
+        /**
+         * @brief Constructs a ReleaseInfoAMD64 object from a JSON document.
+         * @param document The JSON document containing release information.
+         */
         ReleaseInfoAMD64(const rapidjson::Document& document);
         
+        /**
+         * @brief Retrieves a list of supported cloud server releases.
+         * @return A list of CloudServerInfo structure representing supported releases.
+         */
         std::list<CloudServerInfo> get_supported_release_list() const override;
 
+        /**
+         * @brief Retrieves the current Long Term Support (LTS) release.
+         * @return An optional CloudServerInfo structure representing the current LTS release.
+         */
         std::optional<CloudServerInfo> get_current_LTS_release() const override;
 
+        /**
+         * @brief Retrieves the SHA-256 checksum of the disk1.img associated with a given release alias.
+         * @param alias Any alias associated with the server release.
+         * @return An optional string containing the SHA-256 checksum.
+         */
         std::optional<std::string> get_sha256_img(const std::string& alias) const override;
 
-
     private:
-        std::list<CloudServerInfo> cloud_servers;
-
+        /**
+         * @brief Checks and processes AMD64 products from the JSON document.
+         * @param document The JSON document containing product information.
+         */
         void check_amd64_products(const rapidjson::Document& document);
     };
 
